8f5a3d6d45e919e28ed8b5f9fd413cf2553fcb03
cobra <slxviper@yahoo.de>
2009-12-06 18:10:06 +0100

    Initial commit

---
+++ josm-de.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+#
+# Copyright (C) 2009 "Cobra" from <http://www.openstreetmap.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Startup-script for josm:
+#   - aktualisiert josm falls nötig auf die aktuelle Version von latest oder tested (einstellbar)
+#   - sichert alte Versionen (sehr hilfreich, wenn die neue Version nicht funktioniert wie gewünscht)
+#   - erlaubt es, eine alte Version zu starten (per josm -r [revision])
+#   - übergibt alle Argumente an JOSM, nützlich um einen oder mehrere Dateien direkt in JOSM zu öffnen
+#   - setzt Umgebungsvariablen, übergibt die passenden Parameter an Java und sorgt dafür, dass alsa benutzt wird
+#
+# Konfiguration (unter diesem Kommentarblock):
+#   - Verzeichnis für die gespeicherten josm-Versionen anpassen, falls gewünscht
+#   - josm-Variante wählen (latest/tested)
+#   - Anzahl der gesicherten Versionen anpassen, falls gewünscht
+#   - Wird compiz (3D-Desktop) benutzt? Dann bitte die entsprechende Zeile auskommentieren
+#   - Menge des für josm verfügbaren Speichers anpassen
+#   - Wer andere Parameter für java oder josm ändern möchte, kann das in der letzten Zeile tun
+#
+# Benutzung:
+#   josm [-lr] [revision] [DATEI(EN)]
+#
+#   Optionem:
+#   -l	alle gespeicherten josm-Versionen ausgeben und beenden
+#   -r	die angegebene Version von josm starten, als Argument entweder eine (lokal vorhandene) Revisionsnummer angeben oder "last" für die vorletzte gespeicherte
+#
+# ToDo: 
+#   - add possibility to configure a proxy and to select a certain version of java
+#   - add possibility to select an older revision and run it
+#   - add possibility to select -tested instead of -latest revision
+#   - detect automatically if compiz is running
+#   - detect if aoss is available, if not, return warning and start without it
+#   - add some help (e.g. via --help)
+#
+ 
+# Wo soll das josm-archiv gespeichert werden?
+dir=~/bin/josm-archive
+# Welche Variante von josm soll genutzt werden? "tested" ist die neueste mehr oder weniger stabile Version, "latest" kann teilweise schwere Fehler haben, daher ist hier Vorsicht angesagt!
+version=tested
+# Wie viele alte Versionen sollen gesichert werden?
+numbackup=5
+# Zeit in Sekunden, die wget bis zu einem erneuten Versuch bzw. Abbruch warten soll; bei langsamen Verbindungen diesen Wert erhöhen
+timeout=10
+# Maximale Anzahl an Versuchen, eine Verbindung herzustellen
+retries=2
+# Falls compiz eingesetzt wird, muss die nächste Zeile auskommentiert werden
+#export AWT_TOOLKIT=MToolkit
+# Wie viel Speicher soll für josm maximal zur Verfügung stehen?
+mem=1024M
+ 
+cd $dir
+ 
+# parse arguments
+set -- `getopt "hlr:" "$@"` || {
+      echo "Benutzung: `basename $0` [-h] [-l] [-r revision] [Dateien]" 1>&2
+      exit 1
+}
+override_rev=0
+latestrev=-1
+while :
+do
+      case "$1" in
+           -h) echo "Benutzung: `basename $0` [-h] [-l] [-r revision] [Dateien]"; exit 0 ;;
+           -l) echo "Verfügbare josm-Versionen: "; ls josm*.jar | cut -d '-' -f 2 | cut -d '.' -f 1 ; exit 0 ;;
+           -r) shift; override_rev=1; latestrev="$1" ;;
+           --) break ;;
+      esac
+      shift
+done
+shift
+ 
+# parse special revision argument "last" for using next to last revision
+if [ $override_rev -eq 1 -a $latestrev = last ]
+  then
+    latestrev=`ls josm*.jar | cut -d '-' -f 2 | cut -d '.' -f 1 | tail -n 2 | head -n 1`
+fi
+ 
+# if $dir doesn't exist, create it (initial setup):
+if [ -d $dir ]; then :
+  else mkdir -p $dir; echo "Verzeichnis $dir existiert nicht; wird angelegt..."
+fi
+ 
+# get revision number of newest local version:
+if ls josm-*.jar > /dev/null;
+  then latestlocalrev=`ls josm*.jar | cut -d '-' -f 2 | cut -d '.' -f 1 | tail -n 1`
+  else latestlocalrev=0
+fi
+ 
+# get revision number of backed up versions
+oldestrev=`ls josm*.jar | cut -d '-' -f 2 | cut -d '.' -f 1 | head -n 1`
+ 
+# count backed up versions
+numsaved=`ls josm*.jar | grep -c ''`
+ 
+if [ $override_rev -eq 1 ]
+  # check if desired revision is available:
+  then
+    if ls josm*.jar | cut -d '-' -f 2 | cut -d '.' -f 1 | grep $latestrev
+      then
+        echo "Erzwinge Benutzung von Version $latestrev"
+      else
+        echo "Revision $latestrev konnte nicht gefunden werden! `basename $0` -l zeigt eine Liste der verfügbaren Versionen. Ende."
+        exit 1
+    fi
+  else
+    # get revision number of desired version
+    latestrev=`wget -qO - --tries=$retries --timeout=$timeout http://josm.openstreetmap.de/version | grep $version | cut -d ' ' -f 2`
+    if [ ${latestrev:=0} -eq 0 ]
+      then echo "Konnte aktuelle Version nicht vom Server lesen, wechsle in Offline-Modus"
+    fi
+ 
+    # download current revision of josm if newest local revision is older than the current revision of josm on the server
+    if [ $latestrev -eq 0 ]
+      then
+        echo "Offline-Modus, benutze letzte aktuelle Version"
+        latestrev=$oldestrev
+      else
+      if [ $latestlocalrev -lt $latestrev ]
+        then
+          echo "aktuelle lokale Version ist $latestlocalrev, neueste verfügbare Version ist $latestrev - starte download..."
+          wget -O $dir/josm-$latestrev.jar -N http://josm.openstreetmap.de/download/josm-$version.jar
+          # delete oldest file if enough newer ones are present
+          if [ $numsaved -gt $numbackup ]
+            then rm $dir/josm-$oldestrev.jar
+          fi
+        else
+        if [ $latestlocalrev -gt $latestrev ]
+          then
+            echo "aktuelle lokale Version ist neuer als die Version auf dem Server - benutze lokale Version $latestlocalrev"
+            latestrev=$latestlocalrev
+          else
+            echo "lokale Version $latestlocalrev ist bereits aktuell"
+        fi
+      fi
+    fi
+fi
+ 
+# start josm: use alsa instead of oss, enable 2D-acceleration, set maximum amount of memory used for josm to 1024MB and pass all arguments to josm:
+cd $OLDPWD
+echo "starte josm..."
+aoss java -jar -Xmx$mem -Dsun.java2d.opengl=true $dir/josm-$latestrev.jar $@ &
---
+++ josm-en.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+#
+# Copyright (C) 2009 "Cobra" from <http://www.openstreetmap.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Startup-script for josm:
+#   - gets always the newest version of josm-latest.jar or josm-tested.jar (configurable)
+#   - backs up old versions (useful when the new one doesn't work properly)
+#   - is able to launch an old version of josm (via josm -r [revision])
+#   - passes all arguments to josm - you can pass files to open with josm, e.g. 'josm trace0*.gpx trace10.gpx'
+#   - sets environment variables, passes correct parameters to java and use alsa instead of oss
+#
+# configuration (just below these comments):
+#   - change archive-directory if desired
+#   - adjust number of desired backups
+#   - do you use compiz? Then uncomment that line.
+#   - adjust amount of RAM available to josm
+#   - if you want to change or add some parameters for java look at the last line
+#
+# usage:
+#   josm [-lr] [revision] [FILE(S)]
+#
+#   Options:
+#   -l	lists all saved versions of josm and exits
+#   -r	start this revision of josm, revision is either an absolute number or "last" for next to last saved version
+#
+# ToDo: 
+#   - add possibility to configure a proxy and to select a certain version of java
+#   - detect automatically if compiz is running
+#   - detect if aoss is available, if not, return warning and start without it
+#   - add some help (e.g. via --help)
+#
+
+# where should this script save josm-latest.jar to?
+dir=~/bin/josm-archive
+# which version of josm do you want to use? "tested" will work for sure, "latest" may have serious bugs, be careful when using "latest"
+version=latest
+# how many old versions should be backed up?
+numbackup=15
+# timeout for wget in seconds; increase when using a slower connection
+timeout=5
+# how often should wget retry when a timeout occured?
+retries=2
+# fix for use with compiz, uncomment next line if you *do* use compiz
+#export AWT_TOOLKIT=MToolkit
+# how many memory should java assign to josm?
+mem=1024M
+
+cd $dir
+
+# parse arguments
+set -- `getopt "hlr:" "$@"` || {
+      echo "Usage: `basename $0` [-h] [-l] [-r revision] [files]" 1>&2
+      exit 1
+}
+override_rev=0
+latestrev=-1
+while :
+do
+      case "$1" in
+           -h) echo "Usage: `basename $0` [-h] [-l] [-r revision] [files]"; exit 0 ;;
+           -l) echo "available revisions of josm: "; ls josm*.jar | cut -d '-' -f 2 | cut -d '.' -f 1 ; exit 0 ;;
+           -r) shift; override_rev=1; latestrev="$1" ;;
+           --) break ;;
+      esac
+      shift
+done
+shift
+
+# parse special revision argument "last" for using next to last revision
+if [ $override_rev -eq 1 -a $latestrev = last ]
+  then
+    latestrev=`ls josm*.jar | cut -d '-' -f 2 | cut -d '.' -f 1 | tail -n 2 | head -n 1`
+fi
+
+# if $dir doesn't exist, create it (initial setup):
+if [ -d $dir ]; then :
+  else mkdir -p $dir; echo "directory $dir does not exist; creating it..."
+fi
+
+# get revision number of newest local version:
+if ls josm-*.jar > /dev/null;
+  then latestlocalrev=`ls josm*.jar | cut -d '-' -f 2 | cut -d '.' -f 1 | tail -n 1`
+  else latestlocalrev=0
+fi
+
+# get revision number of backed up versions
+oldestrev=`ls josm*.jar | cut -d '-' -f 2 | cut -d '.' -f 1 | head -n 1`
+
+# count backed up versions
+numsaved=`ls josm*.jar | grep -c ''`
+
+if [ $override_rev -eq 1 ]
+  # check if desired revision is available:
+  then
+    if ls josm*.jar | cut -d '-' -f 2 | cut -d '.' -f 1 | grep $latestrev
+      then
+        echo "forcing use of revision $latestrev"
+      else
+        echo "revision $latestrev not found! Use `basename $0` -l to display a list of available revisions. exiting."
+        exit 1
+    fi
+  else
+    # get revision number of desired version
+    latestrev=`wget -qO - --tries=$retries --timeout=$timeout http://josm.openstreetmap.de/version | grep $version | cut -d ' ' -f 2`
+    if [ ${latestrev:=0} -eq 0 ]
+      then echo "could not get version from server, working in offline mode"
+    fi
+
+    # download current revision of josm if newest local revision is older than the current revision of josm on the server
+    if [ $latestrev -eq 0 ]
+      then
+        echo "working offline, using latest local version..."
+        latestrev=$oldestrev
+      else
+      if [ $latestlocalrev -lt $latestrev ]
+        then
+          echo "latest local version is $latestlocalrev, latest available version is $latestrev - starting download..."
+          wget -O $dir/josm-$latestrev.jar -N http://josm.openstreetmap.de/download/josm-$version.jar
+          # delete oldest file if enough newer ones are present
+          if [ $numsaved -gt $numbackup ]
+            then rm $dir/josm-$oldestrev.jar
+          fi
+        else
+        if [ $latestlocalrev -gt $latestrev ]
+          then
+            echo "latest local version is newer than version on the server - using local version $latestlocalrev"
+            latestrev=$latestlocalrev
+          else
+            echo "local version $latestlocalrev is already uptodate"
+        fi
+      fi
+    fi
+fi
+
+# start josm: use alsa instead of oss, enable 2D-acceleration, set maximum amount of memory used for josm to 1024MB and pass all arguments to josm:
+cd $OLDPWD
+echo "starting josm..."
+aoss java -jar -Xmx$mem -Dsun.java2d.opengl=true $dir/josm-$latestrev.jar $@ &
+
